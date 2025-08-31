0b31d557652877ed6c07d95798aeeffbc0d3340f
Ivanko <ivan.dovzhanyn@gmail.com>
2025-08-31 15:50:45 +0300

    Solution

--- src/components/PostDetails.tsx
+++ src/components/PostDetails.tsx
@@ -63,83 +63,78 @@ export const PostDetails: React.FC<Props> = ({ postId }) => {
       {postError && <div className="notification is-danger">{postError}</div>}
       {isLoadingPost}
 
-      {post && (
-        <>
-          <div className="block">
-            <h2 data-cy="PostTitle">
-              #{post.id}: {post.title}
-            </h2>
-            <p data-cy="PostBody">{post.body}</p>
-          </div>
+      <>
+        <div className={`block ${!post ? 'is-hidden' : ''}`}>
+          <h2 data-cy="PostTitle">
+            #{post?.id ?? ''}: {post?.title ?? ''}
+          </h2>
+          <p data-cy="PostBody">{post?.body ?? ''}</p>
+        </div>
 
-          <div className="block">
-            {isLoadingComments && <Loader />}
+        <div className="block">
+          {isLoadingComments && <Loader />}
 
-            {commentsError && (
-              <div className="notification is-danger" data-cy="CommentsError">
-                {commentsError}
-              </div>
-            )}
-
-            {!isLoadingComments && !commentsError && comments.length === 0 && (
-              <p className="title is-4" data-cy="NoCommentsMessage">
-                No comments yet
-              </p>
-            )}
-
-            {!isLoadingComments && comments.length > 0 && (
-              <>
-                <p className="title is-4">Comments:</p>
-
-                {comments.map(comment => (
-                  <article
-                    key={comment.id}
-                    className="message is-small"
-                    data-cy="Comment"
-                  >
-                    <div className="message-header">
-                      <a
-                        href={`mailto:${comment.email}`}
-                        data-cy="CommentAuthor"
-                      >
-                        {comment.name}
-                      </a>
-                      <button
-                        data-cy="CommentDelete"
-                        type="button"
-                        className="delete is-small"
-                        aria-label="delete"
-                        onClick={() => handleDeleteComment(comment.id)}
-                      >
-                        delete button
-                      </button>
-                    </div>
-
-                    <div className="message-body" data-cy="CommentBody">
-                      {comment.body}
-                    </div>
-                  </article>
-                ))}
-              </>
-            )}
-
-            {!isLoadingComments && !commentsError && (
-              <button
-                data-cy="WriteCommentButton"
-                type="button"
-                className="button is-link"
-                onClick={() => setIsFormVisible(prev => !prev)}
-              >
-                {isFormVisible ? 'Cancel' : 'Write a comment'}
-              </button>
-            )}
-          </div>
-
-          {isFormVisible && (
-            <NewCommentForm postId={post.id} onAdd={handleAddComment} />
+          {commentsError && (
+            <div className="notification is-danger" data-cy="CommentsError">
+              {commentsError}
+            </div>
           )}
-        </>
-      )}
+
+          {!isLoadingComments && !commentsError && comments.length === 0 && (
+            <p className="title is-4" data-cy="NoCommentsMessage">
+              No comments yet
+            </p>
+          )}
+
+          {!isLoadingComments && comments.length > 0 && (
+            <>
+              <p className="title is-4">Comments:</p>
+
+              {comments.map(comment => (
+                <article
+                  key={comment.id}
+                  className="message is-small"
+                  data-cy="Comment"
+                >
+                  <div className="message-header">
+                    <a href={`mailto:${comment.email}`} data-cy="CommentAuthor">
+                      {comment.name}
+                    </a>
+                    <button
+                      data-cy="CommentDelete"
+                      type="button"
+                      className="delete is-small"
+                      aria-label="delete"
+                      onClick={() => handleDeleteComment(comment.id)}
+                    >
+                      delete button
+                    </button>
+                  </div>
+
+                  <div className="message-body" data-cy="CommentBody">
+                    {comment.body}
+                  </div>
+                </article>
+              ))}
+            </>
+          )}
+
+          {!isLoadingComments && !commentsError && (
+            <button
+              data-cy="WriteCommentButton"
+              type="button"
+              className="button is-link"
+              onClick={() => setIsFormVisible(prev => !prev)}
+            >
+              {isFormVisible ? 'Cancel' : 'Write a comment'}
+            </button>
+          )}
+        </div>
+
+        {isFormVisible && post && (
+          <NewCommentForm postId={post.id} onAdd={handleAddComment} />
+        )}
+      </>
     </div>
   );
 };
